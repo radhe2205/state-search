86c26786d1ead07bcb11c32fe98f43f453dfb3d0
CSCI B551 Class <git@github.iu.edu:zkachwal>
2021-03-01 17:20:00 -0500

    Correcting test scipt and cases for Part1

--- part3/test_a1p3.py
+++ part3/test_a1p3.py
@@ -1,10 +1,3 @@
-# !/usr/bin/env python3
-# YOU SHOULD NOT MODIFY THIS FILE
-# 
-# test_a1p3.py version 2021.02.24
-#
-# Rushabh Shah
-
 
 '''test_choose_team.py
 
@@ -18,51 +11,59 @@ For final grading we will be using more complex test cases.
 import pytest
 import numpy as np
 import assign
+
+import multiprocessing
+import time
 time_ = 100
 
+
 def get_solution(test_file):
-
+	start_time = time.time()
 	result = []
-
 	for i in assign.solver(test_file):
-
 		result.append([i['assigned-groups'],i['total-cost']])
+		if time.time() - start_time >= 100 :
+			break
 
 	return result
 
 def check_names(test_file,result):
 	names_ = [j for i in [i.split('-') for i in result[0]] for j in i]
-	
+
 	names = set(names_)
 	with  open(test_file,'r') as f:
 		
 		original_names = set()
 		for i in f.readlines():
 			original_names.add(i.split()[0])
-
+	
 	return (original_names==names and len(names)==len(original_names))
 
 def check_solution(test_file,result,threshold = float('inf')):
 
 	assert len(result) != 0, "No solution found"
 
-	assert check_names(test_file,result[-1]) == True, 'Everyone should be assigned to a team'
+	assert result[-1] >= 0, "Score cannot be negative" 
 
-	assert type(result[-1][1]) in (int,float), 'TypeError'
+	assert check_names(test_file,result) == True, 'Everyone should be assigned to a team'
 
-	assert result[-1][1] <= threshold, 'The cost is incorrect, it could be better'
+	assert type(result[1]) in (int,float), 'TypeError'
+
+	assert result[1] <= threshold, 'The cost is incorrect, it could be better'
 
 
-@pytest.mark.timeout(time_)
 def test_case_1():
-	check_solution('test1.txt',get_solution('test1.txt'),10)
+	test_file = 'test1.txt'
+	check_solution('test1.txt',get_solution(test_file)[-1],10)
 
 
-@pytest.mark.timeout(time_)
 def test_case_2():
-	check_solution('test2.txt',get_solution('test2.txt'),15)
+	test_file = 'test2.txt'
+	check_solution(test_file,get_solution(test_file)[-1],15)
 
 
-@pytest.mark.timeout(time_)
 def test_case_3():
-	check_solution('test3.txt',get_solution('test3.txt'))  ## there is no threshold for this case. 
+	test_file = 'test3.txt'
+	check_solution(test_file,get_solution(test_file)[-1])  ## there is no threshold for this case. 
+
+
