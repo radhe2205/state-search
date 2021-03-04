7e80f0e8f888d62f5eac8771a0d11dec227755c4
CSCI B551 Class <git@github.iu.edu:zkachwal>
2021-03-04 01:56:06 -0500

    Final update for test_a1p3. Should resolve all issues. Apologies for the hassle and thank you for being patient! :)

--- part3/test_a1p3.py
+++ part3/test_a1p3.py
@@ -12,56 +12,56 @@ import pytest
 import numpy as np
 import assign
 import time
+import signal
 time_ = 100
 
+def handler(signum, frame):
+	raise Exception("timeout")
 
 def get_solution(test_file):
-	start_time = time.time()
-	result = []
-	for i in assign.solver(test_file):
-		result.append([i['assigned-groups'],i['total-cost']])
-		if time.time() - start_time >= time_ :
-			break
-
-	return result
+	results = []
+	try:
+		for i in assign.solver(test_file):
+			results.append([i['assigned-groups'],i['total-cost']])
+	except Exception:
+		return results
 
 def check_names(test_file,result):
 	names_ = [j for i in [i.split('-') for i in result[0]] for j in i]
-
 	names = set(names_)
 	with  open(test_file,'r') as f:
-		
 		original_names = set()
 		for i in f.readlines():
 			original_names.add(i.split()[0])
-	
 	return (original_names==names and len(names)==len(original_names))
 
 def check_solution(test_file,result,threshold = float('inf')):
-
 	assert len(result) != 0, "No solution found"
-
 	assert result[-1] >= 0, "Score cannot be negative" 
-
 	assert check_names(test_file,result) == True, 'Everyone should be assigned to a team'
-
 	assert type(result[1]) in (int,float), 'TypeError'
-
 	assert result[1] <= threshold, 'The cost is incorrect, it could be better'
 
-@pytest.mark.timeout(time_+5)
 def test_case_1():
+	signal.signal(signal.SIGALRM, handler)
+	signal.alarm(time_)
 	test_file = 'test1.txt'
-	check_solution('test1.txt',get_solution(test_file)[-1],10)
+	results=[[]]
+	results+=get_solution(test_file)
+	check_solution(test_file,results[-1],10) 
 
-@pytest.mark.timeout(time_+5)
 def test_case_2():
+	signal.signal(signal.SIGALRM, handler)
+	signal.alarm(time_)
 	test_file = 'test2.txt'
-	check_solution(test_file,get_solution(test_file)[-1],15)
+	results=[[]]
+	results+=get_solution(test_file)
+	check_solution(test_file,results[-1],15) 
 
-@pytest.mark.timeout(time_+5)
 def test_case_3():
+	signal.signal(signal.SIGALRM, handler)
+	signal.alarm(time_)
 	test_file = 'test3.txt'
-	check_solution(test_file,get_solution(test_file)[-1])  ## there is no threshold for this case. 
-
-
+	results=[[]]
+	results+=get_solution(test_file)
+	check_solution(test_file,results[-1])  ## there is no threshold for this case. 
